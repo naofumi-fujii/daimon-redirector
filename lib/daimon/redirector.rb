97641164f981e50210521e82f0ee9998c81fa1a0
naofumi-fujii <naofumi.fujii@gmail.com>
2020-01-30 14:43:43 +0900

    $ bundle gem .

---
+++ lib/daimon/redirector.rb
@@ -0,0 +1,8 @@
+require "daimon/redirector/version"
+
+module Daimon
+  module Redirector
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/daimon/redirector/version.rb
@@ -0,0 +1,5 @@
+module Daimon
+  module Redirector
+    VERSION = "0.1.0"
+  end
+end
